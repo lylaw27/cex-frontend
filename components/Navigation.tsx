ab0f28c38c92591a0307e24c74e006280e531cc2
Larry Law <69749219+lylaw27@users.noreply.github.com>
2025-05-23 12:24:08 +0800

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,13 +1,22 @@
+import Link from "next/link";
+import GithubIcon from "./githubicon";
+
 export default function Navigation({connect, address}) {
     return (
-        <div className="container mx-auto py-8">
-            <div className="flex justify-between">
+        <div className="container mx-auto py-6">
+            <div className="flex justify-between items-center">
                 <div>
-                    <a href="/">ExchangeGG</a>
+                    <Link href="/">ExchangeGG</Link>
                 </div>
-                <div className="flex space-x-6 align-middle">
-                    <a href={`/portfolio/${address}`}>portfolio</a>
-                    <a>help</a>
+                <div className="flex space-x-6 align-middle items-center">
+                    {
+                        address ? 
+                        <Link href={`/portfolio/${address}`}>My Portfolio</Link>:
+                        <div className="hover:cursor-pointer" onClick={()=>{alert("Connect to Web3 Wallet First!")}}>My Portfolio</div>
+                    }
+                    <Link href="https://github.com/lylaw27/Crypto-Exchange-Java">
+                        <GithubIcon/>
+                    </Link>
                     {address ? <div>{address}</div> : <button onClick={connect} className="p-3 bg-blue-500 font-bold rounded-lg text-white">Connect</button>}
                 </div>
             </div>
